ab6c3c9cfd6baa41c55adda020cefa6ab0247098
Aquarius <deepakjeganathan@gmail.com>
2020-04-12 21:37:21 +0100

    Adapt to ion

--- ion_oneplus3.mk
+++ ion_oneplus3.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2018 The ionOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,13 +20,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oneplus3 device
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ion stuff.
+$(call inherit-product, vendor/ion/config/common_full_phone.mk)
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2019-10-01
 
-PRODUCT_NAME := lineage_oneplus3
+PRODUCT_NAME := ion_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -36,3 +36,18 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys"
 
 TARGET_VENDOR := oneplus
+
+#ion official
+ION_BUILD_TYPE := OFFICIAL
+ION_RELEASE_TYPE := Release
+
+#ion maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ion.maintainer=<deepakjr>
+
+#ion GApps
+TARGET_GAPPS_ARCH := arm64
+
+#ion boot_res
+TARGET_BOOT_ANIMATION_RES := 1080
+
